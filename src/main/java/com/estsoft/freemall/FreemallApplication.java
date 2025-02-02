8d302efeb56a59728839a5463d4bcd3b90205b77
jingeon <ephigh1397@naver.com>
2025-02-02 20:44:52 +0900

    chore: Initial Commit 프로젝트 생성

---
+++ src/main/java/com/estsoft/freemall/FreemallApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft.freemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreemallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estsoft/freemall/FreemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.freemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
