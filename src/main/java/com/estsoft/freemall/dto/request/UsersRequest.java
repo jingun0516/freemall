aca8eba2ee5ad8c260cb4aedd9b480947fdff001
jingeon <ephigh1397@naver.com>
2025-02-03 22:43:36 +0900

    feature: Create UsersRequest DTO
    create UsersRequest for Registering User
    
    #8

---
+++ src/main/java/com/estsoft/freemall/dto/request/UsersRequest.java
@@ -0,0 +1,33 @@
+package com.estsoft.freemall.dto.request;
+
+import com.estsoft.freemall.entity.Users;
+import com.estsoft.freemall.enums.Gender;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UsersRequest {
+    private String loginId;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private LocalDate dateOfBirth;
+    private Gender gender;
+    private String address;
+
+    public Users toEntity() {
+        return Users.builder()
+                .loginId(loginId)
+                .name(name)
+                .email(email)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .dateOfBirth(dateOfBirth)
+                .gender(gender)
+                .address(address).build();
+    }
+}
