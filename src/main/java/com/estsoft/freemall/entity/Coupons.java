34bd371bddd017b44d29d06fee41d2e2b2120c82
jingeon <ephigh1397@naver.com>
2025-02-03 20:45:50 +0900

    feature: Create Entity Coupons
    #5

---
+++ src/main/java/com/estsoft/freemall/entity/Coupons.java
@@ -0,0 +1,49 @@
+package com.estsoft.freemall.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Coupons {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column
+    private String description;
+
+    @Column(name = "discount_rate")
+    private BigDecimal discountRate;
+
+    @Column(name = "discount_price")
+    private BigDecimal discountPrice;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
