9999eccf93ff1c23c3dea595c50264e19e7235ca
jingeon <ephigh1397@naver.com>
2025-02-04 00:43:54 +0900

    feature: Create UsersApiController
    Add Create Users Method
    
    #8

--- src/main/java/com/estsoft/freemall/controller/api/UsersApiController.java
+++ src/main/java/com/estsoft/freemall/controller/api/UsersApiController.java
@@ -1,4 +1,23 @@
 package com.estsoft.freemall.controller.api;
 
+import com.estsoft.freemall.dto.request.UsersRequest;
+import com.estsoft.freemall.entity.Users;
+import com.estsoft.freemall.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UsersApiController {
+    private final UsersService usersService;
+
+    @PostMapping
+    public ResponseEntity<Users> register(@RequestBody UsersRequest request) {
+        return ResponseEntity.ok(usersService.register(request));
+    }
 }
