97a42ec6a455d2284d6984330d396c3f9630f63f
jingeon <ephigh1397@naver.com>
2025-02-03 20:48:00 +0900

    feature: Create Entity UserPaymentMethods
    #5

---
+++ src/main/java/com/estsoft/freemall/entity/UserPaymentMethods.java
@@ -0,0 +1,45 @@
+package com.estsoft.freemall.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(
+        uniqueConstraints = @UniqueConstraint(columnNames = {"payment_method_id", "user_id"})
+)
+public class UserPaymentMethods {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @OneToOne
+    @JoinColumn(name = "payment_method_id", nullable = false)
+    private PaymentMethods paymentMethod;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
