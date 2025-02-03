2784bcbcfb969ea6d2b74771ba82b9c8aa169d3a
jingeon <ephigh1397@naver.com>
2025-02-03 22:45:14 +0900

    feature: Create UsersService Interface
    Create register, login, update,
    softdelete / delete, getAllUsers
    로그인ID / 이메일 중복 확인
    비밀번호변경 추가
    
    #8

--- src/main/java/com/estsoft/freemall/service/UsersService.java
+++ src/main/java/com/estsoft/freemall/service/UsersService.java
@@ -1,4 +1,20 @@
 package com.estsoft.freemall.service;
 
+import com.estsoft.freemall.dto.request.UsersRequest;
+import com.estsoft.freemall.entity.Users;
+
+import java.util.List;
+
 public interface UsersService {
+    Users register(UsersRequest request);
+    Users login(String loginId, String password);
+    Users update(Long userId, UsersRequest request);
+    void softDelete(Long userId);
+    void delete(Long userId);
+    List<Users> getAllUsers();
+
+    boolean isLoginIdDuplicate(String loginId);
+    boolean isEmailDuplicate(String email);
+
+    boolean changePassword(Long userId, String currentPassword, String newPassword);
 }
