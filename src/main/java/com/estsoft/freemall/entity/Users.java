6ee490e56e13935422f123ea0d3cbc4d02cf594c
jingeon <ephigh1397@naver.com>
2025-02-03 20:48:08 +0900

    feature: Create Entity Users
    #5

--- src/main/java/com/estsoft/freemall/entity/Users.java
+++ src/main/java/com/estsoft/freemall/entity/Users.java
@@ -1,4 +1,91 @@
 package com.estsoft.freemall.entity;
 
+import com.estsoft.freemall.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.query.Order;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id", unique = true, nullable = false)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "unique_key")
+    private String uniqueKey;
+
+    @OneToOne
+    @JoinColumn(name = "membership_id")
+    private Membership membership;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserPaymentMethods> userPaymentMethods = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reviews> reviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderHistory> orderHistory = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Inquiries> inquiries = new ArrayList<>();
+
+    @Column(name = "registration_date", nullable = false)
+    private LocalDateTime registrationDate;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "last_login", nullable = false)
+    private LocalDateTime lastLogin;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.registrationDate = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateLastLoginAt() {
+        this.lastLogin = LocalDateTime.now();
+    }
 }
