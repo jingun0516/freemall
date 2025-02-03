5118816f2de3ac40b8be9600ecbf7671880b9a63
jingeon <ephigh1397@naver.com>
2025-02-04 00:41:55 +0900

    feature: disable Security
    #7

--- src/main/java/com/estsoft/freemall/config/SecurityConfig.java
+++ src/main/java/com/estsoft/freemall/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.estsoft.freemall.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
-
-
+    @Bean
+    public WebSecurityCustomizer ignore() {
+        return WebSecurity -> WebSecurity.ignoring()
+                .anyRequest();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
