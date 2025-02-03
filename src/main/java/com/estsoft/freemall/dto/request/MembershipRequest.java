0b495fe6c7af00f0ce6dce019cda52daaeedc14c
jingeon <ephigh1397@naver.com>
2025-02-04 00:45:30 +0900

    feature: create MembershipRequest for register
    #9

---
+++ src/main/java/com/estsoft/freemall/dto/request/MembershipRequest.java
@@ -0,0 +1,16 @@
+package com.estsoft.freemall.dto.request;
+
+import com.estsoft.freemall.entity.Membership;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MembershipRequest {
+    private String level;
+    private String benefits;
+
+    public Membership toEntity() {
+        return new Membership(level, benefits);
+    }
+}
