145fb6551e651f0fa47ab9426363d92d7e9cfb47
jingeon <ephigh1397@naver.com>
2025-02-03 23:07:37 +0900

    feature: implement UsersService
    #8

---
+++ src/main/java/com/estsoft/freemall/service/impl/UsersServiceImpl.java
@@ -0,0 +1,92 @@
+package com.estsoft.freemall.service.impl;
+
+import com.estsoft.freemall.dto.request.UsersRequest;
+import com.estsoft.freemall.entity.Users;
+import com.estsoft.freemall.repository.UsersRepository;
+import com.estsoft.freemall.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsersServiceImpl implements UsersService {
+    private final UsersRepository usersRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public Users register(UsersRequest request) {
+        Users user = request.toEntity();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return usersRepository.save(user);
+    }
+
+    @Override
+    public Users login(String loginId, String password) {
+        Users user = usersRepository.findByLoginIdAndDeletedAtIsNotNull(loginId);
+        if(passwordEncoder.matches(password,user.getPassword())){
+            user.updateLastLoginAt();
+            return user;
+        }
+        return null;
+    }
+
+    @Override
+    public Users update(Long userId, UsersRequest request) {
+        Users user = usersRepository.findByIdAndDeletedAtIsNotNull(userId);
+        if(user == null) {
+            return null;
+        }
+        return usersRepository.save(request.updateEntity(user));
+    }
+
+    @Override
+    public Users softDelete(Long userId) {
+        Users user = usersRepository.findByIdAndDeletedAtIsNotNull(userId);
+        if(user == null) {
+            return null;
+        }
+        user.setDeletedAt(LocalDateTime.now());
+        return usersRepository.save(user);
+    }
+
+    @Override
+    public boolean delete(Long userId) {
+        Users user = usersRepository.findByIdAndDeletedAtIsNotNull(userId);
+        if(user == null) {
+            return false;
+        }
+        usersRepository.delete(user);
+        return true;
+    }
+
+    @Override
+    public List<Users> getAllUsers() {
+        return usersRepository.getAllByDeletedAtIsNotNull();
+    }
+
+    @Override
+    public boolean isLoginIdDuplicate(String loginId) {
+        return usersRepository.existsByLoginIdAndDeletedAtIsNotNull(loginId);
+    }
+
+    @Override
+    public boolean isEmailDuplicate(String email) {
+        return usersRepository.existsByEmailAndDeletedAtIsNotNull(email);
+    }
+
+    @Override
+    public Users changePassword(Long userId, String currentPassword, String newPassword) {
+        Users user = usersRepository.findByIdAndDeletedAtIsNotNull(userId);
+        if(user == null) {
+            return null;
+        }
+        if(passwordEncoder.matches(currentPassword,user.getPassword())) {
+            user.setPassword(passwordEncoder.encode(newPassword));
+        }
+        return usersRepository.save(user);
+    }
+}
