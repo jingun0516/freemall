c1051ef6a87db6d7e6deab2deed70bf9345d0fff
jingeon <ephigh1397@naver.com>
2025-02-04 01:15:06 +0900

    feature: create MembershipApiController
    Create Membership Register
    
    #9

---
+++ src/main/java/com/estsoft/freemall/controller/api/MembershipApiController.java
@@ -0,0 +1,23 @@
+package com.estsoft.freemall.controller.api;
+
+import com.estsoft.freemall.dto.request.MembershipRequest;
+import com.estsoft.freemall.entity.Membership;
+import com.estsoft.freemall.service.MembershipService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/membership")
+@RequiredArgsConstructor
+public class MembershipApiController {
+    private final MembershipService membershipService;
+
+    @PostMapping
+    public ResponseEntity<Membership> register(@RequestBody MembershipRequest reuqest) {
+        return ResponseEntity.ok(membershipService.register(reuqest));
+    }
+}
