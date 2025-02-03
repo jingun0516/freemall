f7af3a8f5f66f5c7a5cc2ad2571103e818bf9821
jingeon <ephigh1397@naver.com>
2025-02-03 20:46:46 +0900

    feature: Create Entity OrderItems
    #5

---
+++ src/main/java/com/estsoft/freemall/entity/OrderItems.java
@@ -0,0 +1,33 @@
+package com.estsoft.freemall.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+public class OrderItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private OrderHistory orderHistory;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Products product;
+
+    @Column
+    private Integer quantity;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+}
