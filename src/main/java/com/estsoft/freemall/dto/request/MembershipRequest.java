155960e83ce7b399b77ca14ed52d6708077159e2
jingeon <ephigh1397@naver.com>
2025-02-04 17:33:15 +0900

    feature: create method update Membership
    #9

--- src/main/java/com/estsoft/freemall/dto/request/MembershipRequest.java
+++ src/main/java/com/estsoft/freemall/dto/request/MembershipRequest.java
@@ -13,4 +13,13 @@ public class MembershipRequest {
     public Membership toEntity() {
         return new Membership(level, benefits);
     }
+    public Membership updateEntity(Membership membership) {
+        if(level != null) {
+            membership.setLevel(level);
+        }
+        if(benefits != null) {
+            membership.setBenefits(benefits);
+        }
+        return membership;
+    }
 }
