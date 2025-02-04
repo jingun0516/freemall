c857a3e527442d6c866a33364ce17fd1c7e82cb3
jingeon <ephigh1397@naver.com>
2025-02-04 18:49:03 +0900

    feature: Create CategoriesService
    addCategory
    findCategoryByName
    
    #10

---
+++ src/main/java/com/estsoft/freemall/service/CategoriesService.java
@@ -0,0 +1,9 @@
+package com.estsoft.freemall.service;
+
+import com.estsoft.freemall.dto.request.CategoriesRequest;
+import com.estsoft.freemall.entity.Categories;
+
+public interface CategoriesService {
+    Categories addCategory(CategoriesRequest request);
+    Categories findCategoryByName(String name);
+}
