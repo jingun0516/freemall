12bd394f94b11ef4cce505fa1cdca2564adf929f
jingeon <ephigh1397@naver.com>
2025-02-05 00:43:50 +0900

    feature: Create SellersRequest
    #12

---
+++ src/main/java/com/estsoft/freemall/dto/request/SellersRequest.java
@@ -0,0 +1,17 @@
+package com.estsoft.freemall.dto.request;
+
+import com.estsoft.freemall.entity.Sellers;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SellersRequest {
+    private String storeName;
+    private String storeDescription;
+
+    public Sellers toEntity() {
+        return new Sellers(storeName, storeDescription);
+    }
+
+}
