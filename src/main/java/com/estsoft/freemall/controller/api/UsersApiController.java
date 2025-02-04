1211e6e2ada37873ba55662ebd3d8a5c4bcb8d32
jingeon <ephigh1397@naver.com>
2025-02-05 00:47:43 +0900

    feature: create getById to UsersApi
    #8

--- src/main/java/com/estsoft/freemall/controller/api/UsersApiController.java
+++ src/main/java/com/estsoft/freemall/controller/api/UsersApiController.java
@@ -5,10 +5,7 @@ import com.estsoft.freemall.entity.Users;
 import com.estsoft.freemall.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,5 +16,10 @@ public class UsersApiController {
     @PostMapping
     public ResponseEntity<Users> register(@RequestBody UsersRequest request) {
         return ResponseEntity.ok(usersService.register(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<Users> getById(@RequestParam("id") Long id) {
+        return ResponseEntity.ok(usersService.getById(id));
     }
 }
