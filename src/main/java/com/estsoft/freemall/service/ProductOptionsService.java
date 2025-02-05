d9e4375a89502ba3ee863a4fd913e4d3cd46c535
jingeon <ephigh1397@naver.com>
2025-02-05 17:38:01 +0900

    feature: Create ProductOptionsService
    #16

---
+++ src/main/java/com/estsoft/freemall/service/ProductOptionsService.java
@@ -0,0 +1,8 @@
+package com.estsoft.freemall.service;
+
+import com.estsoft.freemall.dto.request.ProductOptionsRequest;
+import com.estsoft.freemall.entity.ProductOptions;
+
+public interface ProductOptionsService {
+    ProductOptions addProductOptions(ProductOptionsRequest request);
+}
