30e3af09e203bfc8e4cf6010f85aa1e63ebed11d
jingeon <ephigh1397@naver.com>
2025-02-05 15:32:28 +0900

    feature: create DiscountsRequest
    #17

---
+++ src/main/java/com/estsoft/freemall/dto/request/DiscountsRequest.java
@@ -0,0 +1,26 @@
+package com.estsoft.freemall.dto.request;
+
+import com.estsoft.freemall.entity.Discounts;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class DiscountsRequest {
+    private Long productId;
+    private BigDecimal discountRate;
+    private BigDecimal discountPrice;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public Discounts toEntity() {
+        return Discounts.builder()
+                .discountRate(discountRate)
+                .discountPrice(discountPrice)
+                .startDate(startDate)
+                .endDate(endDate).build();
+    }
+}
