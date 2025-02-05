8d3b874635994b936fa5f029b85f3acce97c39c1
jingeon <ephigh1397@naver.com>
2025-02-05 17:37:52 +0900

    feature: Create ProductOptionsRequest
    #16

---
+++ src/main/java/com/estsoft/freemall/dto/request/ProductOptionsRequest.java
@@ -0,0 +1,16 @@
+package com.estsoft.freemall.dto.request;
+
+import com.estsoft.freemall.entity.ProductOptions;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductOptionsRequest {
+    private long productId;
+    private String attribute;
+
+    public ProductOptions toEntity() {
+        return new ProductOptions(attribute);
+    }
+}
