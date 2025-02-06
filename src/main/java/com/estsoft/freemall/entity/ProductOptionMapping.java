0c8f42999efbe9762ca351ee460d437f7978dcb9
jingeon <ephigh1397@naver.com>
2025-02-06 18:45:20 +0900

    feature: Create ProductOptionMapping Entity
    #21

---
+++ src/main/java/com/estsoft/freemall/entity/ProductOptionMapping.java
@@ -0,0 +1,31 @@
+package com.estsoft.freemall.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductOptionMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Products product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_option_id", nullable = false)
+    private ProductOptions productOption;
+
+    public ProductOptionMapping(Products product, ProductOptions productOption) {
+        this.product = product;
+        this.productOption = productOption;
+    }
+}
