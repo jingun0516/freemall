42c3c6ff28521c4d4961fe8890054f0dc3f47130
jingeon <ephigh1397@naver.com>
2025-02-07 00:13:34 +0900

    feature: add Methods PaymentMethodsAPI
    addPaymentMethodProvider
    
    #24

--- src/main/java/com/estsoft/freemall/controller/api/PaymentMethodsApiController.java
+++ src/main/java/com/estsoft/freemall/controller/api/PaymentMethodsApiController.java
@@ -1,7 +1,10 @@
 package com.estsoft.freemall.controller.api;
 
+import com.estsoft.freemall.dto.request.PaymentMethodProvidersRequest;
 import com.estsoft.freemall.dto.request.PaymentMethodsRequest;
+import com.estsoft.freemall.entity.PaymentMethodProviders;
 import com.estsoft.freemall.entity.PaymentMethods;
+import com.estsoft.freemall.service.PaymentMethodProvidersService;
 import com.estsoft.freemall.service.PaymentMethodsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/payment-method")
 public class PaymentMethodsApiController {
     private final PaymentMethodsService paymentMethodsService;
+    private final PaymentMethodProvidersService paymentMethodProvidersService;
 
     @PostMapping
     public ResponseEntity<PaymentMethods> addPaymentMethod(@RequestBody PaymentMethodsRequest request) {
         return ResponseEntity.ok(paymentMethodsService.addPaymentMethod(request));
+    }
+
+    @PostMapping("/provider")
+    public ResponseEntity<PaymentMethodProviders> addPaymentMethodProvider(@RequestBody PaymentMethodProvidersRequest request) {
+        return ResponseEntity.ok(paymentMethodProvidersService.addPaymentMethodProvider(request));
     }
 }
