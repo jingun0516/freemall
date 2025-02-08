3269f3a10e584ee8cc009054cbe5be8e9da85400
jingeon <ephigh1397@naver.com>
2025-02-09 02:26:27 +0900

    feature: implement methods to DiscountsService
    getDiscounts
    calculateDiscountedPrice
    
    #17

--- src/main/java/com/estsoft/freemall/service/impl/DiscountsServiceImpl.java
+++ src/main/java/com/estsoft/freemall/service/impl/DiscountsServiceImpl.java
@@ -9,6 +9,11 @@ import com.estsoft.freemall.service.ProductsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class DiscountsServiceImpl implements DiscountsService {
@@ -25,5 +30,44 @@ public class DiscountsServiceImpl implements DiscountsService {
         discount.setProduct(product);
 
         return discountsRepository.save(discount);
+    }
+
+    @Override
+    public List<Discounts> getDiscounts(Long productId) {
+        List<Discounts> discounts = discountsRepository.findByProductId(productId);
+        List<Discounts> results = new ArrayList<>();
+
+        for(Discounts discount : discounts) {
+            LocalDateTime startDate = discount.getStartDate();
+            LocalDateTime endDate = discount.getEndDate();
+            if(LocalDateTime.now().isAfter(startDate) && LocalDateTime.now().isBefore(endDate)) {
+                results.add(discount);
+            }
+        }
+
+        return results;
+    }
+
+    @Override
+    public BigDecimal calculateDiscountedPrice(BigDecimal price, List<Discounts> discounts) {
+        BigDecimal maxDiscountRate = BigDecimal.ZERO;
+        BigDecimal maxDiscountAmount = BigDecimal.ZERO;
+
+        for(Discounts discount : discounts) {
+            if(discount.getDiscountRate().compareTo(maxDiscountRate) > 0) {
+                maxDiscountRate = discount.getDiscountRate();
+            }
+            if(discount.getDiscountPrice().compareTo(maxDiscountAmount) > 0) {
+                maxDiscountAmount = discount.getDiscountPrice();
+            }
+        }
+
+        BigDecimal priceAfterRate = price.subtract(maxDiscountAmount);
+        BigDecimal priceAfterAmount = price.subtract(price.multiply(maxDiscountRate));
+        if(priceAfterAmount.compareTo(priceAfterRate) > 0) {
+            return priceAfterAmount;
+        }
+
+        return priceAfterRate;
     }
 }
