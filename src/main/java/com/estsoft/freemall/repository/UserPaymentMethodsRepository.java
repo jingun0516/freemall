484b172c9bc902533d6492888f27764a6e8865b7
jingeon <ephigh1397@naver.com>
2025-02-08 23:37:24 +0900

    feature: add findByUserId to UserPaymentMethodsRepository
    #26

--- src/main/java/com/estsoft/freemall/repository/UserPaymentMethodsRepository.java
+++ src/main/java/com/estsoft/freemall/repository/UserPaymentMethodsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPaymentMethodsRepository extends JpaRepository<UserPaymentMethods, Long> {
+    UserPaymentMethods findByUserId(Long userId);
 }
