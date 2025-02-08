020b65e498da0b94f84432ae97b644606348d4f9
jingeon <ephigh1397@naver.com>
2025-02-08 23:39:42 +0900

    feature: create OrderHistoryService
    addOrderHistory
    
    #29

---
+++ src/main/java/com/estsoft/freemall/service/OrderHistoryService.java
@@ -0,0 +1,8 @@
+package com.estsoft.freemall.service;
+
+import com.estsoft.freemall.dto.request.OrderHistoryRequest;
+import com.estsoft.freemall.entity.OrderHistory;
+
+public interface OrderHistoryService {
+    OrderHistory addOrderHistory(Long userId, OrderHistoryRequest request);
+}
