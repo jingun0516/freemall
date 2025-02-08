1ef261fe8c94f1c56bbc7456dcdaf53f18d7d2ea
jingeon <ephigh1397@naver.com>
2025-02-09 02:25:49 +0900

    feature: add findByProductId to DiscountsRepository
    #17

--- src/main/java/com/estsoft/freemall/repository/DiscountsRepository.java
+++ src/main/java/com/estsoft/freemall/repository/DiscountsRepository.java
@@ -4,6 +4,9 @@ import com.estsoft.freemall.entity.Discounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DiscountsRepository extends JpaRepository<Discounts, Long> {
+    List<Discounts> findByProductId(Long productId);
 }
