53ea4dee0c97af5f0084db3d28cc82ac612d2d54
jingeon <ephigh1397@naver.com>
2025-02-09 02:27:46 +0900

    feature: create OrderItemsService
    addOrderItem
    
    #30

---
+++ src/main/java/com/estsoft/freemall/service/OrderItemsService.java
@@ -0,0 +1,8 @@
+package com.estsoft.freemall.service;
+
+import com.estsoft.freemall.dto.request.OrderItemsRequest;
+import com.estsoft.freemall.entity.OrderItems;
+
+public interface OrderItemsService {
+    OrderItems addOrderItem(Long orderHistoryId, OrderItemsRequest request);
+}
