873630a24cbb55aac35ae307dd2fc04af2b1a41c
jingeon <ephigh1397@naver.com>
2025-02-08 23:39:03 +0900

    feature: create OrderHistoryRequest
    결제수단Id / 배송지 / 배송비 / 송장번호 / 교환환불정보
    / 주문자 연락처 / 주문메모
    
    #29

---
+++ src/main/java/com/estsoft/freemall/dto/request/OrderHistoryRequest.java
@@ -0,0 +1,37 @@
+package com.estsoft.freemall.dto.request;
+
+import com.estsoft.freemall.entity.OrderHistory;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class OrderHistoryRequest {
+    // 결제수단
+    private Long userPaymentMethodId;
+    // 배송지
+    private String shippingAddress;
+    // 배송비
+    private BigDecimal shippingCost;
+    // 송장 번호 (배송중일 시)
+    private String trackingNumber;
+    // 교환 환불 정보
+    private String cancellationRefundInfo;
+    // 주문자 연락처
+    private String contactInfo;
+    // 주문 메모
+    private String orderNotes;
+
+    public OrderHistory toEntity() {
+        return OrderHistory.builder()
+                .shippingAddress(shippingAddress)
+                .shippingCost(shippingCost)
+                .trackingNumber(trackingNumber)
+                .cancellationRefundInfo(cancellationRefundInfo)
+                .contactInfo(contactInfo)
+                .orderNotes(orderNotes)
+                .build();
+    }
+}
