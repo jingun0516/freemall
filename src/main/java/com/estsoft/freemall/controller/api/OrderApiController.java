df6553c51bd66bee6843b00afb598ca3d75db3ba
jingeon <ephigh1397@naver.com>
2025-02-09 20:51:23 +0900

    feature: add api to OrderApiController
    get
    delete
    
    #29

--- src/main/java/com/estsoft/freemall/controller/api/OrderApiController.java
+++ src/main/java/com/estsoft/freemall/controller/api/OrderApiController.java
@@ -5,10 +5,7 @@ import com.estsoft.freemall.entity.OrderHistory;
 import com.estsoft.freemall.service.OrderHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +16,16 @@ public class OrderApiController {
     @PostMapping
     public ResponseEntity<OrderHistory> addOrder(@RequestBody OrderHistoryRequest request) {
         return ResponseEntity.ok(orderHistoryService.addOrderHistory(8L, request));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderHistory> getOrder(@PathVariable Long orderId) {
+        return ResponseEntity.ok(orderHistoryService.getOrderHistoryById(orderId));
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
+        orderHistoryService.deleteOrderHistoryById(orderId);
+        return ResponseEntity.noContent().build();
     }
 }
