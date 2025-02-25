778f99ea1181580bc921b44d31e35deca950801a
jingeon <ephigh1397@naver.com>
2025-02-25 23:32:10 +0900

    feature: Add Membership RUD API
    #9

--- src/main/java/com/estsoft/freemall/controller/api/MembershipApiController.java
+++ src/main/java/com/estsoft/freemall/controller/api/MembershipApiController.java
@@ -5,10 +5,7 @@ import com.estsoft.freemall.entity.Membership;
 import com.estsoft.freemall.service.MembershipService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/membership")
@@ -19,5 +16,26 @@ public class MembershipApiController {
     @PostMapping
     public ResponseEntity<Membership> register(@RequestBody MembershipRequest reuqest) {
         return ResponseEntity.ok(membershipService.addMembership(reuqest));
+    }
+
+    @GetMapping("/{membershipId}")
+    public ResponseEntity<Membership> getMembership(@PathVariable Long membershipId) {
+        return ResponseEntity.ok(membershipService.getMembershipById(membershipId));
+    }
+
+    @PutMapping("/{membershipId}")
+    public ResponseEntity<Membership> updateMembership(@PathVariable Long membershipId, @RequestBody MembershipRequest reuqest) {
+        return ResponseEntity.ok(membershipService.updateMembership(membershipId, reuqest));
+    }
+
+    @DeleteMapping("/{membershipId}")
+    public ResponseEntity<Void> deleteMembership(@PathVariable Long membershipId) {
+        boolean isDeleted = membershipService.deleteMembership(membershipId);
+
+        if (!isDeleted) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.noContent().build();
     }
 }
